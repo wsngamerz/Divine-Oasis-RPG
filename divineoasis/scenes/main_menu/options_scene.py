2580e442ad96dd2fa615f0eb1df4cff7613a2297
William Neild <wneild5@gmail.com>
2019-06-25 10:31:49 +0100

    Added back button to Options + Added back button + Added blank button to dev assets
    TODO: replace back button with styled one when get back to desktop
    rather than shitty £10 linux laptop

--- divineoasis/scenes/main_menu/options_scene.py
+++ divineoasis/scenes/main_menu/options_scene.py
@@ -33,19 +33,37 @@ class OptionsScene(Scene):
         self.images = {}
         self.sprites = {}
 
+        self.button_coords = {
+            "back_button": [(512, 104), (768, 40)]
+        }
+
     def start_scene(self):
         self.load_sprite_images()
         self.draw_foreground()
 
     def load_sprite_images(self):
-        self.images["back_button"] = self.assets.get_pyglet_image("lang.user_interface.back_button")
+        self.images["back_button_image"] = self.assets.get_pyglet_image("lang.user_interface.back_button")
 
     def draw_foreground(self):
-        pass
+        self.sprites["back_sprite"] = Sprite(self.images["back_button_image"],
+                x=(self.window.width//2) - (self.images["back_button_image"].width//2),
+                y=(self.window.height//2) - 320,
+                batch=self.batch, group=self.foreground)
 
     def on_mouse_motion(self, x: int, y: int, dx: int, dy: int):
         cursor = self.window.get_system_mouse_cursor(self.window.CURSOR_DEFAULT)
+
+        if x >= self.button_coords["back_button"][0][0] and x <= self.button_coords["back_button"][1][0]:
+            if y <= self.button_coords["back_button"][0][1] and y >= self.button_coords["back_button"][1][1]:
+                cursor = self.window.get_system_mouse_cursor(self.window.CURSOR_HAND)
+
         self.window.set_mouse_cursor(cursor)
+
+    def on_mouse_press(self, x: int, y: int, button: int, modifiers: int):
+        if x >= self.button_coords["back_button"][0][0] and x <= self.button_coords["back_button"][1][0]:
+            if y <= self.button_coords["back_button"][0][1] and y >= self.button_coords["back_button"][1][1]:
+                self.logger.debug("Clicked Back Button")
+                self.switch_sub_scene("MenuScene")
 
     def update(self, dt: float):
         self.window.clear()
